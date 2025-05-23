d1700a7740b215674417a06be2d4cc90d5e14f52
hyunjnn <chju0113@naver.com>
2025-05-23 16:10:46 +0900

    Initial commit

---
+++ src/main/java/org/example/ringlesubject/RingleSubjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.ringlesubject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingleSubjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RingleSubjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ringlesubject/RingleSubjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ringlesubject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RingleSubjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
